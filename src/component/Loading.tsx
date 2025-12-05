7e8ea8bccd8c277255ff161065e109a1e64af75b
Abhishek <136466988+Abhishe58@users.noreply.github.com>
2025-12-05 12:23:28 +0530

    Add files via upload

---
+++ src/component/Loading.tsx
@@ -0,0 +1,13 @@
+import "./Loading.css";
+
+export default function Loading() {
+  return (
+    <>
+      <div className="LoadingContainer">
+        <h1>Ratan Industries</h1>
+        <p>Precision in Every Drop…</p>
+        <div className="loadingBox"></div>
+      </div>
+    </>
+  );
+}
