ad3b6c9858af455ac1c7fe67f8e4dca7205cd620
Abhishek <136466988+Abhishe58@users.noreply.github.com>
2025-12-04 22:58:27 -0800

    Update Industries.tsx

--- src/component/Industries.tsx
+++ src/component/Industries.tsx
@@ -119,8 +119,8 @@ export default function Industries() {
   );
 }
 const styles = {
-  loadingcontainer: {
-    position: "fixed",
+   loadingcontainer: {
+    position: "fixed" as const, // tells TS this exact string is valid
     top: 0,
     left: 0,
     width: "100vw",
